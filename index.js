85686d11d9765c0e7e0755bb77fa06ebd27cc22a
michael <michaelyoussef396@gmail.com>
2023-11-21 17:04:22 +1100

    finished

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+function submitData(name, email) {
+    const userData = {
+        name: name,
+        email: email,
+    };
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(userData),
+    };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            document.body.innerHTML += `<p>New ID: ${data.id}</p>`;
+            return data;
+        })
+        .catch(function (error) {
+            document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+            return error.message;
+        });
+}
